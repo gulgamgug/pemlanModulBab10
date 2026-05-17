0c83ab5abd2d5b4633891d062268e72ecf11b106
gulgamgug <gulagaramguguru@gmail.com>
2026-05-17 10:46:50 +0700

    first commit

---
+++ src/TulisFile.java
@@ -0,0 +1,81 @@
+import java.io.File;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.util.Scanner;
+
+/*Buat kode program untuk menampilkan nama dari semua file yang ada di dalam
+suatu direktori. Petunjuk: gunakan perulangan dan method list() atau
+listFiles(). */
+
+public class TulisFile {
+    public static void main(String[] args) {
+        var keyboard = new Scanner(System.in);
+        System.out.print("Masukkan teks yang akan disimpan: ");
+        var text = keyboard.nextLine();
+
+        try (var writer = new FileWriter("test.txt", true)) {
+            writer.write(text);
+        } catch (IOException e) {
+            System.err.println("Gagal menulis ke file");
+        }
+
+        File file = new File("test.txt");
+        showFileSize(file);
+        listIsiFolder(file);
+        
+    }
+    static void showFileSize(File file) {
+        long length = file.length();
+        double size;
+        String satuan;
+        if (length>=1024*1024) {
+            size = (double) length/ (1024*1024);
+            satuan = " MB";
+        } else if (length>=1024) {
+            size = (double) length/1024;
+            satuan = " KB";
+        } else {
+            size = (double) length;
+            satuan = " B";
+        }
+        System.out.println("file size: " + size + satuan);
+    }
+    static void listIsiFolder(File file) {
+        File pathFolder = file.getAbsoluteFile().getParentFile();
+        String[] isiFolder = pathFolder.list();
+        System.out.println("\nIsi folder " + file.getAbsoluteFile().getParent() + ":");
+        for (String nama : isiFolder) {
+            System.out.println(nama);
+        }
+    }
+    static void hapusFolder() {
+    File folder = new File("FolderContoh");
+    if (!folder.exists()) {
+        folder.mkdir();
+        System.out.println("Folder contoh berhasil dibuat.");
+    }
+    for (int i = 1; i <= 3; i++) {
+        File fileDummy = new File(folder, "File" + i + ".txt");
+        try {
+            if (fileDummy.createNewFile()) {
+                System.out.println(fileDummy.getName() + " berhasil dibuat.");
+            }
+        } catch (IOException e) {
+            System.err.println("Error: " + e.getMessage());
+        }
+    }
+
+    System.out.println("Menghapus folder " + folder.getName());
+
+    File[] daftarFile = folder.listFiles();
+    if (daftarFile != null) {
+        for (File f : daftarFile) {
+            boolean fileTerhapus = f.delete();
+            System.out.println("Operasi hapus" + f.getName() + ": " + (fileTerhapus ? "sukses" : "gagal"));
+        }
+    }
+
+    boolean folderTerhapus = folder.delete();
+    System.out.println("Menghapus folder " + (folderTerhapus ? "sukses" : "gagal"));
+}
+}
